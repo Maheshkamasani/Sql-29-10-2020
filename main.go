afccacc556be50ae62818f3cda93296b78601d3c
MAHESWAR REDDY KAMASANI <73190083+Maheshkamasani@users.noreply.github.com>
2020-10-30 21:02:42 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Println("Go Mysqldatabase")
+
+	db, err := sql.Open("mysql", "username:Mahesh143@amma@tcp(127.0.0.1:3306)/demo")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+}
